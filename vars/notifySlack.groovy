463d0aeb37fce35aa85da652b1d5318865af9935
Anuj Jain <98390204+AnujjainWD@users.noreply.github.com>
2025-07-30 09:38:16 +0530

    Delete vars directory

--- vars/notifySlack.groovy
+++
@@ -1,6 +0,0 @@
-def call(String message, String color = 'good') {
-    slackSend(
-        color: color,
-        message: "${env.JOB_NAME} - #${env.BUILD_NUMBER} - ${message}"
-    )
-}
--- vars/qualityCheck.groovy
+++
@@ -1,5 +0,0 @@
-def call() {
-    echo "Running static code analysis..."
-    sh './gradlew check' // or mvn verify, etc
-}
-
