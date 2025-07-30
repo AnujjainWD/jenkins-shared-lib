6b76cc2726108e38e41398ff152cf88ff5b631c8
Anuj Jain <98390204+AnujjainWD@users.noreply.github.com>
2025-07-30 20:33:47 +0530

    Update first_resource.groovy

--- vars/first_resource.groovy
+++ vars/first_resource.groovy
@@ -2,5 +2,11 @@ import org.utils.Hello
 
 def call(String name = "Stranger") {
     def message = Hello.greet(name)
-    echo message
+    
+    def currentTime = new Date().format("yyyy-MM-dd HH:mm:ss", TimeZone.getTimeZone('IST'))
+    def jobName = env.JOB_NAME
+
+    echo "${message}"
+    echo " Executed at: ${currentTime}"
+    echo " Jenkins Job: ${jobName}"
 }
